5b054238b6439ec6eaae64684968e76f9cc51696
wardenlym <wardenlym@gmail.com>
2021-03-17 14:11:52 +0800

    renew for my archlinux

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # dotfiles
+my dotfiles
+
+```bash
+git clone https://github.com/wardenlym/dotfiles ~/.dotfiles
+./dotfiles/setup.sh
+```
+
+### TODO
+renew vim config
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+ln -s ~/.dotfiles/zshrc ~/.zshrc
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+#ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+#ln -s ~/.dotfiles/vim ~/.vim
